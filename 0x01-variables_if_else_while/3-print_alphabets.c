86536739682f879a77a68f7a53ec634e35f3ec66
Patience-Ifeoluwapo <ifeoluwapo.babatunde@gmail.com>
2022-09-10 04:57:04 -0700

    upper case and lower case

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - print the alphabet in upper, case and then in lower case.
+ * main - print the alphabet in upper case and then in lower case.
  *
  * Description: using the main function
  * this program prints "Programming is lower case, or upper case"
@@ -9,10 +9,12 @@
 int main(void)
 {
 	char ch;
+
 	for (ch = 'a' ; ch <= 'z' ; ch++)
 	{
 		putchar(ch);
 	}
+
 	for (ch = 'A' ; ch <= 'Z' ; ch++)
 	{putchar(ch);
 	}
