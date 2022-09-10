05ab01fd9ab2d00d913c9b276713985ab3bc7535
Patience-Ifeoluwapo <ifeoluwapo.babatunde@gmail.com>
2022-09-10 05:17:53 -0700

    lower case

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - print the alphabet in lower case
+ *
+ * this program prints "Programming is lower case"
+ * Return: 0
+ */
+int main(void)
+{
+	char;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		if (ch ! = 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+	}
+	putchar('\n');
+	return (0)
+}
