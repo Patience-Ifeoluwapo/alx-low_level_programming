34b960a3e542dbd8dad4e3538eff3bf1712cc09b
Patience-Ifeoluwapo <ifeoluwapo.babatunde@gmail.com>
2022-09-10 08:47:30 -0700

    smile

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print lower case alphabet in reverse.
+ *
+ * Description: using the main function.
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		puchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
