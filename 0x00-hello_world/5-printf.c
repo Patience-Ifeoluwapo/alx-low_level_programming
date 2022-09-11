2a0fd35694676452f097fe961f8b54f5e089f215
Patience-Ifeoluwapo <ifeoluwapo.babatunde@gmail.com>
2022-09-10 20:35:36 -0700

    hello p

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/** main - print the string in the puts function
+/** main - print with proper grammar, but the outcome is a piece of art.
 *
 * Description: using the main function
 * this program prints "with proper grammar, but the outcome is a piece of art"
@@ -8,6 +8,8 @@
 */
 int main(void)
 {
+	/*
 	printf("with proper grammar, but the outcome is a piece of art,\n");
+	*/
 	return (0);
 }
