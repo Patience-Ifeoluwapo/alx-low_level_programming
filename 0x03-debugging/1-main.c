8b3153e8386d5ef819c554e4dcac409192cb147c
Patience-Ifeoluwapo <ifeoluwapo.babatunde@gmail.com>
2022-09-16 20:31:08 -0700

    infinite loop

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
@@ -19,7 +18,7 @@ int main(void)
 	/*putchar(i);*/
 	/*}*/
 
-	printf('Infinite loop avoided! \\o/\n");
+	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
 }
