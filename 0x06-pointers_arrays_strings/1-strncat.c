e80fc05aa46167982dfb082761d72dff86415152
Patience-Ifeoluwapo <ifeoluwapo.babatunde@gmail.com>
2022-09-22 06:10:41 -0700

    concatenation of two strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,14 +14,18 @@ char *_strncat(char *dest, char *src, int n)
 	int length, j;
 
 	length = 0;
+	j = 0;
+
 	while (dest[length] != '\0')
 	{
 		length++;
 	}
+
 	for (j = 0; j < n && src[j] != '\0'; j++, length++)
 	{
 		dest[length] = src[j];
 	}
+
 	dest[length] = '\0';
 
 	return (dest);
