de0fe0734964323a5981714be7d6af356dfcbcfa
Patience-Ifeoluwapo <ifeoluwapo.babatunde@gmail.com>
2022-09-22 05:21:19 -0700

    My header file

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(inc c);
+int _putchar(int c);
 
 char *_strcat(char *dest, char *src);
 
@@ -9,7 +9,7 @@ char *_strncat(char *dest, char *src, int n);
 
 char *_strncpy(char *dest, char *src, int n);
 
-int _strcmp(char s1, char *s2);
+int _strcmp(char *s1, char *s2);
 
 void reverse_array(int *a, int n);
 
@@ -21,7 +21,7 @@ char *leet(char *);
 
 char *rot13(char *);
 
-void print_numbers(int n);
+void print_number(int n);
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
