3c80c09f0450aa045d08b38ac87a4dfdcbd82979
Patience-Ifeoluwapo <ifeoluwapo.babatunde@gmail.com>
2022-09-30 13:34:58 -0700

    change coins

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -9,7 +9,7 @@
 int main(int argc, char *argv[])
 {
 	/*Declaring variables*/
-	int position, total, change, aux = 0;
+	int position, total, change, aux;
 	int coins[] = {25, 10, 5, 2, 1}; /*Array int*/
 
 	position = total = change = aux = 0;
@@ -27,6 +27,7 @@ int main(int argc, char *argv[])
 		printf("0\n");
 		return (0);
 	}
+
 	/*Declaring While*/
 	while (coins[positions] != '\0')
 	{
