e6ecc32379fd1dced640658661935ad5f2a2d818
Patience-Ifeoluwapo <ifeoluwapo.babatunde@gmail.com>
2022-10-06 15:33:54 -0700

    Allocate memory

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * malloc_checked - A function that allocates memory using malloc.
+ * @b: integer variable that holds the memory size
+ * Author: Patience-Ifeoluwapo
+ * Return: 0 means program was successful
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+Now, am beginning to understand the concept of pointers and dynamic memory allocation in c programming.
