3aa7f5afc963aea792d73493de066479686433a7
Patience-Ifeoluwapo <ifeoluwapo.babatunde@gmail.com>
2022-10-22 03:41:40 -0700

    My Header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
 /**
- * struct dog - user define data type
+ * struct dog - user defined data type
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
@@ -14,7 +14,7 @@ struct dog
 	char *owner;
 };
 typedef struct dog dog_t;
-void init_dog(struct dog *d, char *name, float age, char *owmer);
+void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog (char *name, float age, char *owner);
 void free_dog(dog_t *d);
