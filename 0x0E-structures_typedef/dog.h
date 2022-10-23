70cc330936c5983d65f84173320cabfe5614e379
Patience-Ifeoluwapo <ifeoluwapo.babatunde@gmail.com>
2022-10-22 21:56:21 -0700

    My header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
+
 /**
  * struct dog - user defined data type
  * @name: name of the dog
@@ -18,4 +19,5 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog (char *name, float age, char *owner);
 void free_dog(dog_t *d);
+
 #endif
