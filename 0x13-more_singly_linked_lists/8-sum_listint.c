3324e050c706c93f62408a565a15a3ffbb5d4a6d
Patience-Ifeoluwapo <ifeoluwapo.babatunde@gmail.com>
2022-10-25 15:53:04 -0700

    sum list

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -4,8 +4,7 @@
  * @head: pointer to the first node in the linked list
  * Return: sum of element data in the linked list
  */
-
-int sum_listint(listint_t *head);
+int sum_listint(listint_t *head)
 {
 	int sum = 0;
 	listint_t *temp = head;
